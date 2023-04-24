121d4609c0ae25d0d007e76a5a9fb4794944c2a9
ludovicjj <jahanlud@gmail.com>
2023-04-24 17:54:53 +0200

    Add service db with mysql image and connect to bdd with PDO

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
-echo 'hello world';
+// Récupération des variables d'env pour la connexion
+$host = getenv('MYSQL_HOST');
+$dbname = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+
+// PDO
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion à la base de données $dbname réussie !";
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+}
+
 phpinfo();
